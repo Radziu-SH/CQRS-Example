abb05ccb5bfc1f0d81a3f5a2d732ca721b051721
Radosław Grochowicz <grochowicz.radoslaw@protonmail.com>
2023-12-14 15:26:11 +0100

    init task #1

---
+++ CQRS-Example.Infrastructure/Repository/TestRepository.cs
@@ -0,0 +1,13 @@
+﻿using CQRS_Emaple.Domain.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQRS_Example.Infrastructure.Repository
+{
+    public class TestRepository : ITestRepository
+    {
+    }
+}
